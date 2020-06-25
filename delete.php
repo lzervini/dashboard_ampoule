36c81ff3fb758d8baae9f984bdd99b2aca6e6b95
lzervini <lezervinia@gmail.com>
2020-06-25 10:56:28 +0200

    Add sql to delete

---
+++ delete.php
@@ -0,0 +1,12 @@
+<?php require_once('database.php');
+
+    if(isset($_GET['id'])){
+        $sql = 'delete from ampoule where id =:id';
+        $sth = $dbh->prepare($sql);
+        $sth->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+        $sth -> execute();
+
+    }
+    header('Location: index.php');
+
+?>
