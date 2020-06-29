ec6750c03af54c1d7324d06e32149760d44c28d5
lzervini <lezervinia@gmail.com>
2020-06-29 10:36:57 +0200

    Add favicon

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/ico" href="images/lightbulb.ico" />
 
 </head>
 <body>
