137e14ffbf4e8b6b465f8fbd2352bcf9d2f9ef7b
lzervini <lezervinia@gmail.com>
2020-06-29 09:44:52 +0200

    Add div container

--- index.php
+++ index.php
@@ -27,8 +27,6 @@
             <th scope="col">Supprimer</th>
         </tr>
 </thead>
-        <div>
-    </div>
 
     <?php
 
@@ -56,6 +54,7 @@
 
 ?>
     </table>
+</div>
 
     <?php
     if (count($result)===0){
@@ -63,8 +62,9 @@
         }
 
     ?>
-    
+    <div class="container ">
     <a href="modify.php"><button type="submit" class="add btn btn-outline-primary rounded-pill offset-lg-10 col-lg-2 "><img src="images/add.png" alt="signe plus">Ajouter</img></button></a>
+    </div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
