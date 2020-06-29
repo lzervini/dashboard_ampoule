50fe981f2a5a3b672376eecb3e65194bbe22eb6f
lzervini <lezervinia@gmail.com>
2020-06-29 11:35:38 +0200

    Remove placeholder in<select>

--- modify.php
+++ modify.php
@@ -120,12 +120,12 @@
     <form action='' method='POST' class=" offset-lg-3 col-lg-6 p-3  ">
         
     <div class="pb-3">
-            Date du changement d'ampoule: <input type="date" name='date_changement' id='date_changement' placeholder="Date" value="<?=$dateChange; ?>" class="form-control ">
+            Date du changement d'ampoule: <input type="date" name='date_changement' id='date_changement' value="<?=$dateChange; ?>" class="form-control ">
         </div>
         
         <div class="pb-3">
             Numéro de l'étage :
-            <select type="text" name='etage' id='etage' placeholder="Étage" value="<?=$etage; ?>" class="form-control">
+            <select name='etage' id='etage' class="form-control">
                 <option value="RDC">RDC</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -168,7 +168,6 @@
 
     </form>
             </div>
- </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
