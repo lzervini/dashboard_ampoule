72a93fd4b0c2539126b68dd297d8f0829c734af0
lzervini <lezervinia@gmail.com>
2020-06-29 12:02:24 +0200

    test

--- index.php
+++ index.php
@@ -61,9 +61,8 @@
 
     ?>
     
-    <div class="container">
         <a href="modify.php" class="px-3 pz-1 btn btn-outline-primary rounded-pill"><img src="images/add.png" alt="signe plus">Ajouter</a>
-    </div>
+
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
