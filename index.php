bfeadae68c46ce2f81ab49ee86ec84fc4bb79100
lzervini <lezervinia@gmail.com>
2020-06-29 16:50:49 +0200

    Add form

--- index.php
+++ index.php
@@ -8,6 +8,21 @@
     <title>Login</title>
 </head>
 <body>
+    <div id="container">
+            
+            <form action=".php" method="POST">
+                <h1>Connexion</h1>
+                
+                <label>Nom d'utilisateur</label>
+                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+
+                <label>Mot de passe</label>
+                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+                <input type="submit" id='submit' value='login' >
+
+            </form>
+        </div>
     
 </body>
 </html>
