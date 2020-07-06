192d14eb70f087cb23b3614bc8ec5fbf61688954
lzervini <lezervinia@gmail.com>
2020-07-06 10:17:21 +0200

    Remove cdn

--- accueil.php
+++ accueil.php
@@ -14,7 +14,6 @@ if(empty($_SESSION['username'])){
     <title>Dashboard </title>
     <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/ico" href="images/logo.ico" />
 
