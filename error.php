d63730a774b2f22a78355c07c28150caf2ac7793
lzervini <lezervinia@gmail.com>
2020-12-17 16:39:11 +0100

    Add redirected page

---
+++ error.php
@@ -0,0 +1,21 @@
+    <?php
+        header('refresh: 4; url=index.php');
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Redirection ...</title>
+</head>
+<body>
+    <div class="container d-flex justify-content-center align-items-center mt-5">
+        <div class="d-flex flex-column align-items-center ">
+            <h1 class='text-danger'> Identifiants incorrects </h1>
+            <p> Vous allez être redirigé vers la page de connexion.</p>
+        </div>
+    </div>
+</body>
+</html>
